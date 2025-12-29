bea95f0a8926b35599f932740e952ce99576a2ad
iliece <ilieces@gmail.com>
2025-12-29 22:03:27 +0900

    NULL  초기화 설명

--- pointer2.c
+++ pointer2.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main(){
   int a=0,b=0,c=0;
-  int*ip = NULL; //초기화
- //ip=&a;
+//int*ip = NULL; //초기화
+  int*ip = &a;
+//ip=&a;
   *ip=10;
   printf("%d %d %d %d\n",a,b,c,*ip);
+
   ip=&b;
   *ip=20;
-  printf("%d%d%d\n",a,b,c,*ip);
+  printf("%d %d %d\n",a,b,c,*ip);
     return 0;
 }
