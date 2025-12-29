437cea3e0aa7d8a0e7f75ce5236b6335c2800d05
iliece <ilieces@gmail.com>
2025-12-29 22:27:06 +0900

    주소값출력추가

--- array_2level1.c
+++ array_2level1.c
@@ -2,6 +2,7 @@
 int main(){
     int array[2][3] = {{1,2,3},{4,5,6,}};
     printf("%p %p %p\n",&array[0][0],&array[0][1],&array[0][2]);
-     printf("%p %p %p\n",&array[1][0],&array[1][1],&array[1][2]);
+    printf("%p %p %p\n",&array[1][0],&array[1][1],&array[1][2]);
+    printf("%d %d \n",array[0][0],array[0][1]); //주소값출력
 return 0;
 }
